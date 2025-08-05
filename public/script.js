414d8645cc1aea9e8addebfdaab1e43effa1b94c
fsdqLaurio <b2029fsdqlaurio@pshs.edu.ph>
2025-08-05 08:44:59 +0800

    first ng

--- public/script.js
+++ public/script.js
@@ -1,1 +1,9 @@
-// no scrit tag needed
+// no script tag needed
+
+let age = 14;
+let maxAge = 100;
+let numPerDay = 70;
+
+let totalRequired = (numPerDay*365)*(maxAge-age);
+
+document.write("You will need " + totalRequired," to last you until the ripe old age of " + maxAge )
