8d4d59fcb880e6117d9fa07d008f7718b9562ac3
fsdqLaurio <b2029fsdqlaurio@pshs.edu.ph>
2025-08-12 08:22:22 +0800

    Non-Graded Exercise Second Version

--- public/script.js
+++ public/script.js
@@ -1,9 +1,21 @@
 // no script tag needed
 
-let age = 14;
-let maxAge = 100;
-let numPerDay = 2;
-
+let age = window.prompt('Enter your age', 14);
+let maxAge = window.prompt('Enter your Max age', 80);
+let numPerDay = window.prompt('Enter a number per day', 2);
 let totalRequired = (numPerDay*365)*(maxAge-age);
 
-document.write("You will need " + totalRequired," hashbrowns to last you until the ripe old age of " + maxAge )
+
+if(confirm(`Is ${maxAge}, ${age}, ${numPerDay} correct?`)){
+    console.log("You will need " + totalRequired," of your favorite to last you until the ripe old age of " + maxAge)
+    document.write("You will need " + totalRequired," of your favorite to last you until the ripe old age of " + maxAge)
+}
+
+else{
+    console.log('Click to run the code again');
+    
+}
+
+
+
+
