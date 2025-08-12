80b21aa44e562f1e8222e0ff1a6b5114bc052138
fsdqLaurio <b2029fsdqlaurio@pshs.edu.ph>
2025-08-12 08:49:58 +0800

    Speed Test 2

--- public/script.js
+++ public/script.js
@@ -1,20 +1,20 @@
 // no script tag needed
 
-let age = window.prompt('Enter your age', 14);
-let maxAge = window.prompt('Enter your Max age', 80);
-let numPerDay = window.prompt('Enter a number per day', 2);
-let totalRequired = (numPerDay*365)*(maxAge-age);
+// let age = window.prompt('Enter your age', 14);
+// let maxAge = window.prompt('Enter your Max age', 80);
+// let numPerDay = window.prompt('Enter a number per day', 2);
+// let totalRequired = (numPerDay*365)*(maxAge-age);
 
 
-if(confirm(`Is ${maxAge}, ${age}, ${numPerDay} correct?`)){
-    console.log("You will need " + totalRequired," of your favorite to last you until the ripe old age of " + maxAge)
-    document.write("You will need " + totalRequired," of your favorite to last you until the ripe old age of " + maxAge)
-}
+// if(confirm(`Is ${maxAge}, ${age}, ${numPerDay} correct?`)){
+    // console.log("You will need " + totalRequired," of your favorite to last you until the ripe old age of " + maxAge)
+    // document.write("You will need " + totalRequired," of your favorite to last you until the ripe old age of " + maxAge)
+// }
 
-else{
-    console.log('Click to run the code again');
+// else{
+   // console.log('Click to run the code again');
     
-}
+// }
 
 
 
